01fd406206c140cb7d4031286a91e600a514978c
cdeppert <cod314@lehigh.edu>
2015-01-24 19:36:07 +0000

    WelcomeClass program

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,18 @@
+//Connor Deppert
+//CSE-02-112
+//Welcome Message (HW01)
+//25 January 2015
+
+public class WelcomeClass {
+    public static void main(String [] args) {
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<-C--O--D--3--1--4->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("This past summer I spent two weeks in California. I visited Disneyland, San Diego, and other parts of southern California.");
+    }
+}
